27edeaad0064bdf60b2e89bd7131c925837b7f2d
isabelle qga <isabelleqga@hotmail.com>
2022-10-13 11:10:05 -0300

    comentarios adicionados pro codigo ficar legivel

--- cliente.py
+++ cliente.py
@@ -12,22 +12,30 @@ identificador = "0"
 chave = -1
 
 while True:
+    # digita a mensagem que será enviada posteriormente
     mensagem = input("Digite>>")
 
     if identificador == "0":
+        # se o cliente nao tiver um identificador oferecido pelo servidor
+
         print("O cliente não possui identificador.")
+        # envia uma mensagem padrao, basicamente dizendo "nao tenho identificador"
         mClientSocket.send("comunica".encode())
+        # recebe o identificador
         identificador = mClientSocket.recv(2048)
         identificador = identificador.decode()
         print(f"Identificador recebido: {identificador}")
+        # diz que recebeu o identificador
         mClientSocket.send("Identificador recebido.".encode())
 
+        # começa o DH. recebe as chaves publicas do servidor
         chavesPublicasString = mClientSocket.recv(2048)
         chavesPublicasString = chavesPublicasString.decode()
         chavesPublicas = chavesPublicasString.split(',')
 
         commonPaint = int(chavesPublicas[0])
         base = int(chavesPublicas[1])
+        # gera sua chave privada com o random
         aColor = random.randint(2, 64)
         acMix = int(pow(base,aColor,commonPaint))  
         acMix = str(acMix)
@@ -38,16 +46,20 @@ while True:
         mClientSocket.send(acMix.encode()) 
         commonSecretA = int(pow(bcMix,aColor,commonPaint))
 
+        # chave do DH definida pro cliente. o msm processo aconteceu no servidor e ele guardou essa chave com o identificador
         chave = commonSecretA
 
     else:
+        # o cliente já tem identificador pq se comunicou antes com o servidor
         print(f"Identificador do cliente: {identificador}")
+        # envia o identificador pro servidor saber quem ele é e usar a chave DH certa pra comunicação
         mClientSocket.send(identificador.encode())
 
+    # criptografando a mesangem que foi digitada no inicio e enviando
     msgCriptografada = cryptocode.encrypt(mensagem, str(chave))
     mClientSocket.send(msgCriptografada.encode())
 
-
+    # recebendo resposta do servidor
     data = mClientSocket.recv(2048)
     reply = data.decode()
     msgDescriptografada = cryptocode.decrypt(reply, str(chave))
--- server_autenticacao/servidor_autenticacao.py
+++ server_autenticacao/servidor_autenticacao.py
@@ -10,24 +10,27 @@ import uuid
 
 def HandleRequest(mClientSocket, mClientAddr, dic):
     while True:
-
+        # recebendo o 'oi' do cliente. o identificador dele ou dizendo q nao tem
         data = mClientSocket.recv(2048)
         identificadorBase = data.decode()
 
         if identificadorBase == "comunica":
+            # se o cliente nao tem identificador
             identificadorCliente = uuid.uuid4()
             identificadorCliente = str(identificadorCliente)
             print(f'Conexão do cliente com o identificador {identificadorCliente}.')
-            #dic[identificadorCliente] = mClientAddr
+            # criou o identificador e mandou
             mClientSocket.send(identificadorCliente.encode()) 
-            resposta = mClientSocket.recv(2048)
+            resposta = mClientSocket.recv(2048) # confirmacao de q o cliente recebeu o id
 
+            # inicio de DH. servidor manda as chaves publicas pro cliente
             chavesPublicasString = "23, 9"
             mClientSocket.send(chavesPublicasString.encode()) 
-            
-            chavesPublicas = [23, 9]
+
+            chavesPublicas = chavesPublicasString.split(',')
             commonPaint = chavesPublicas[0]
             base = chavesPublicas[1] 
+            # chave privada do servidor... nao vai guardar isso, vai guardar so o commonsecret
             bColor = random.randint(2, 64)
             bcMix = int(pow(base,bColor,commonPaint))  
             bcMix = str(bcMix)
@@ -38,31 +41,32 @@ def HandleRequest(mClientSocket, mClientAddr, dic):
             acMix = int(acMix.decode())
             commonSecretB = int(pow(acMix,bColor,commonPaint))
 
+            # guardando o identificador do cliente e o commonsecret entre eles q possibilita o DH
             dic[identificadorCliente] = commonSecretB
             chave = dic[identificadorCliente]
 
         else:
+            # se o cliente tinha identificador, foi isso que ele mandou
             identificadorCliente = identificadorBase
             if dic[identificadorCliente] is not None:
                 print(f'Conexão do cliente já conhecido com o identificador {identificadorCliente}.')
+                # servidor entende quem é o cliente e pega a chave DH referente a ele
                 chave = dic[identificadorCliente]
 
+        # comunicacao da parte de realmente receber a mensagem
         mensangemRecebida = mClientSocket.recv(2048)
         req = mensangemRecebida.decode()
         msgDescriptografada = cryptocode.decrypt(req, str(chave))
         print(f'Mensagem recebida: {msgDescriptografada}')
 
+        # respondendo
         msgCriptografada = cryptocode.encrypt("Mensagem recebida com sucesso.", str(chave))
         mClientSocket.send(msgCriptografada.encode())
 
 
-
-
 mSocketServer = socket(AF_INET, SOCK_STREAM)
 print(f'Socket criado ...')
-
 mSocketServer.bind(('127.0.0.1',1235))
-
 mSocketServer.listen()
 
 dic = {}
