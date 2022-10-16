f838517a42f121beadb04e403d5a12711b72e7bb
Thiago <thiagorb2000@gmail.com>
2022-10-15 23:21:34 -0300

    Inserindo o envio de arquivos e as verificações de erros

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -13,14 +13,16 @@ chave = -1
 
 while True:
     # digita a mensagem que será enviada posteriormente
-    mensagem = str(input("Digite>>"))
+    mensagem = input("Digite>>")
 
     if identificador == "0":
         # se o cliente nao tiver um identificador oferecido pelo servidor
 
         print("O cliente não possui identificador.")
         # envia uma mensagem padrao, basicamente dizendo "nao tenho identificador"
-        mClientSocket.send("comunica".encode())
+        envio = "comunica"
+        enviofinal = envio.encode()
+        mClientSocket.send(enviofinal)
         # recebe o identificador
         identificador = mClientSocket.recv(2048)
         identificador = identificador.decode()
@@ -37,13 +39,13 @@ while True:
         base = int(chavesPublicas[1])
         # gera sua chave privada com o random
         aColor = random.randint(2, 64)
-        acMix = int(pow(base,aColor,commonPaint))  
+        acMix = int(pow(base,aColor,commonPaint))
         acMix = str(acMix)
         #recebe o mix
         bcMix = mClientSocket.recv(2048)
         bcMix = int(bcMix.decode())
         #transporta o mix
-        mClientSocket.send(acMix.encode()) 
+        mClientSocket.send(acMix.encode())
         commonSecretA = int(pow(bcMix,aColor,commonPaint))
 
         # chave do DH definida pro cliente. o msm processo aconteceu no servidor e ele guardou essa chave com o identificador
@@ -55,38 +57,48 @@ while True:
         # envia o identificador pro servidor saber quem ele é e usar a chave DH certa pra comunicação
         mClientSocket.send(identificador.encode())
 
-    # criptografando a mesangem que foi digitada no inicio e enviando
+    # criptografando a mensagem que foi digitada no inicio e enviando
     msgCriptografada = cryptocode.encrypt(mensagem, str(chave))
     mClientSocket.send(msgCriptografada.encode())
 
     # recebendo resposta do servidor
+    # data = mClientSocket.recv(2048)
+    # reply = data.decode()
+    # msgDescriptografada = cryptocode.decrypt(reply, str(chave))
+    # print(f'Resposta do servidor: {msgDescriptografada}')
 
-    with open(mensagem, 'wb') as file: #abrir arquivo
+    # recebendo resposta (confirmação) do servidor
+    resp = mClientSocket.recv(2048)
+    resposta = resp.decode()
+    print('CÓDIGO DE RESPOSTA ABAIXO:')
+    print(resposta)
+
+    if resposta == 'Requisição bem-sucedida, objeto requisitado será enviado.':
+        pergunta = 1
+        break
+
+    else:
+        pergunta = int(input('Digite 1 para tentar outra requisição ou 0 para fechar: '))
+        if pergunta == 0:
+            break
+
+if pergunta != 0:
+    with open(mensagem, 'wb') as file:
         while 1:
-            print("0")
-            data = mClientSocket.recv(1000000)
-            print(data)
-            # reply = data.decode()
-            #msgDescriptografada = cryptocode.decrypt(data, chave)
-            #print(f'Resposta do servidor: {msgDescriptografada}')
+            # recebendo arquivo do servidor
+            data = mClientSocket.recv(2048)
             if not data:
                 break
-            #dado = msgDescriptografada.encode()
-            file.write(data)
-    
+            reply = data.decode()
+            msgDescriptografada = cryptocode.decrypt(reply, str(chave))
+            print(f'Resposta do servidor: {msgDescriptografada}')
+            print(msgDescriptografada[2:-1])
+            dataBytes = msgDescriptografada.encode()
+            print(dataBytes)
+            file.write(dataBytes)
+
     print(f'{mensagem} recebido!\n')
 
-
-'''
-namefile = str(input("Arquivo> "))
-
-mClientSocket.send(namefile.encode())
-
-with open(namefile, 'wb') as file: #abrir arquivo
-    while True:
-        data = mClientSocket.recv(1000000) #receber arquivos grandes
-        if not data:
-            break
-        file.write(data)
-        
-print(f" {namefile} recebido!\n")'''
+else:
+    print("Conexão finalizada!")
+    mClientSocket.close()
--- server_autenticacao/servidor_autenticacao.py
+++ server_autenticacao/servidor_autenticacao.py
@@ -7,6 +7,7 @@ from threading import Thread
 import random
 import cryptocode
 import uuid
+import os
 
 def HandleRequest(mClientSocket, mClientAddr, dic):
     while True:
@@ -20,22 +21,22 @@ def HandleRequest(mClientSocket, mClientAddr, dic):
             identificadorCliente = str(identificadorCliente)
             print(f'Conexão do cliente com o identificador {identificadorCliente}.')
             # criou o identificador e mandou
-            mClientSocket.send(identificadorCliente.encode()) 
+            mClientSocket.send(identificadorCliente.encode())
             resposta = mClientSocket.recv(2048) # confirmacao de q o cliente recebeu o id
 
             # inicio de DH. servidor manda as chaves publicas pro cliente
             chavesPublicasString = "23, 9"
-            mClientSocket.send(chavesPublicasString.encode()) 
+            mClientSocket.send(chavesPublicasString.encode())
 
             chavesPublicas = chavesPublicasString.split(',')
             commonPaint = int(chavesPublicas[0])
             base = int(chavesPublicas[1])
             # chave privada do servidor... nao vai guardar isso, vai guardar so o commonsecret
             bColor = random.randint(2, 64)
-            bcMix = int(pow(base,bColor,commonPaint))  
+            bcMix = int(pow(base,bColor,commonPaint))
             bcMix = str(bcMix)
             #transporta o mix
-            mClientSocket.send(bcMix.encode()) 
+            mClientSocket.send(bcMix.encode())
             #recebe o mix
             acMix = mClientSocket.recv(2048)
             acMix = int(acMix.decode())
@@ -59,20 +60,51 @@ def HandleRequest(mClientSocket, mClientAddr, dic):
         msgDescriptografada = cryptocode.decrypt(req, str(chave))
         print(f'Mensagem recebida: {msgDescriptografada}')
 
-        # respondendo
-        with open(msgDescriptografada, 'rb') as file: #abrir arquivo
-            for data in file.readlines():
-                print("for")
-                #msgCriptografada = cryptocode.encrypt(data, chave)
-                mClientSocket.send(data)
-            print("Arquivo enviado!")
+        # RESPONDENDO
+        # msgCriptografada = cryptocode.encrypt("Mensagem recebida com sucesso.", str(chave))
+        # mClientSocket.send(msgCriptografada.encode())
 
+        # Tratamento de sintaxe
+        sintaxe = msgDescriptografada.split(".")
+        tipos_de_arquivo = ['html', 'htm', 'css', 'js', 'png', 'jpg', 'svg', 'pdf', 'jpeg', 'mp4', 'doc', 'zip', 'txt']
+
+        # Verifica como o cliente escreveu o nome do arquivo
+        if '.' not in msgDescriptografada or sintaxe[1] not in tipos_de_arquivo:
+            print('ERRO 400 Bad Request: Mensagem de requisição não entendida pelo servidor.')
+            msgError = 'ERRO 400 Bad Request: Mensagem de requisição não entendida pelo servidor.'
+            data = msgError.encode()
+            mClientSocket.send(data)
+
+        else:
+            # Tratamento de existência de arquivo
+            caminho = "C:/Users/thiag/PycharmProjects/pythonProject1/servidor/"
+            caminhoTodo = caminho + msgDescriptografada
+
+            if os.path.isfile(caminhoTodo):
+                print('Requisição bem-sucedida, objeto requisitado será enviado.')
+                msgSucess = 'Requisição bem-sucedida, objeto requisitado será enviado.'
+                data = msgSucess.encode()
+                mClientSocket.send(data)
+
+                with open(msgDescriptografada, 'rb') as file:
+                    for data in file.readlines():
+                        msgCriptografada = cryptocode.encrypt(str(data), str(chave))
+                        mClientSocket.send(msgCriptografada.encode())
+                        # mClientSocket.send(data)
+                    print('Arquivo enviado!')
+                    break
+
+            else:
+                print('ERRO 404 Not Found! Documento requisitado não localizado no servidor.')
+                msgError = 'ERRO 404 Not Found! Documento requisitado não localizado no servidor.'
+                data = msgError.encode()
+                mClientSocket.send(data)
 
 
 mSocketServer = socket(AF_INET, SOCK_STREAM)
 print(f'Socket criado ...')
 mSocketServer.bind(('127.0.0.1',1235))
-mSocketServer.listen(1)
+mSocketServer.listen()
 
 dic = {}
 
@@ -80,13 +112,5 @@ while True:
     clientSocket, clientAddr =  mSocketServer.accept()
     Thread(target=HandleRequest, args=(clientSocket, clientAddr, dic)).start()
     mSocketServer.close()
-
-
-
-
-
-
-
-
 
    
